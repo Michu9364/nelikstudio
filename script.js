53a304a0ac24e1ec6fa8b0f85f436f7f85e68627
Michu9364 <michal.olearczyk@o2.pl>
2023-06-06 18:26:27 +0200

    Update 06/06

--- script.js
+++ script.js
@@ -2,37 +2,58 @@
 /* Rozwijanie menu na mobile*/
 
 function navbar() {
-    var x = document.getElementById("navmobile");
-    if (x.className.indexOf("slidenavbarshow") == -1) {
-      x.className += " slidenavbarshow";
-    } else { 
-      x.className = x.className.replace(" slidenavbarshow", "");
-    }
+  var x = document.getElementById("navmobile");
+  if (x.className.indexOf("slidenavbarshow") == -1) {
+    x.className += " slidenavbarshow";
+  } else { 
+    x.className = x.className.replace(" slidenavbarshow", "");
+  }
 }
 
 /* Animacja guzika menu na mobile*/
 
 function animatednav(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 }
 
 /* Ładowanie stron*/
 
 function include(elem) {
-    var page = elem.val;
-    $.ajax({
-      url: "fragments/link" + page + ".html",
-      type: "GET"
-    }).done(function(msg) {
-      $('#included_page').html(msg);
-    })
+  var page = elem.val;
+  $.ajax({
+    url: "fragments/link" + page + ".html",
+    type: "GET"
+  }).done(function(msg) {
+    $('#included_page').html(msg);
+  })
+}
+
+/* Ciemny motyw */
+
+function enableDarkMode() {
+  var element = document.body;
+  element.classList.add("dark-mode");
+  Cookies.set('darkMode', 'true');
+}
+
+function disableDarkMode() {
+  var element = document.body;
+  element.classList.remove("dark-mode");
+  Cookies.set('darkMode', 'false');
+}
+
+function toggleDarkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  var isDarkModeEnabled = element.classList.contains("dark-mode");
+  Cookies.set('darkMode', isDarkModeEnabled.toString());
 }
 
 /* Usunięcie watermarka na stronie - domena*/
 
 document.addEventListener('DOMContentLoaded', () => {
-    var disclaimer = document.querySelector("img[alt='www.000webhost.com']");
-    if(disclaimer) {
-      disclaimer.remove();
-    }
+  var disclaimer = document.querySelector("img[alt='www.000webhost.com']");
+  if(disclaimer) {
+    disclaimer.remove();
+  }
 });
