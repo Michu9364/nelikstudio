d9fc835c9ff1830fb6efe1650bda4fac3a4140eb
Michu9364 <michal.olearczyk@o2.pl>
2023-06-23 02:12:40 +0200

    Update 23/06

--- fragments/send-script.php
+++ fragments/send-script.php
@@ -41,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 </body>
             </html>";
 
-        if (mail($mailToSend, "Wiadomość ze strony - " . date("d-m-Y"), $message, $headers)) {
+        if (mail($mailToSend, "Wiadomość ze strony NelikStudio.me - " . date("d-m-Y"), $message, $headers)) {
             $return["status"] = "ok";
         } else {
             $return["status"] = "error";
